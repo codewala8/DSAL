602dad9d45f435d2bec849d7550335a74172223d
codewala8 <167911670+codewala8@users.noreply.github.com>
2024-05-09 08:31:50 +0530

    Update emplyoeeF12.cpp

--- emplyoeeF12.cpp
+++ emplyoeeF12.cpp
@@ -113,3 +113,7 @@ int main()
     } while (choice != 4);
     return 0;
 }
+Company maintains employee information as employee ID, name, designation and salary
+    . Allow user to add, delete information of employee. Display information of particular employee.
+    If employee does not exist an appropriate message is displayed.
+    If it is, then the system displays the employee details. Use index sequential file to maintain the data.
